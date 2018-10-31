4de85ed29c1059b1ea86d5b8db182868fe4e67dd
Cody Brunner <rockchalkwushock@icloud.com>
2018-10-31 11:20:32 -0700

    adds custom file uploading, envar injection, netlify settings.

--- next.config.js
+++ next.config.js
@@ -1,2 +1,29 @@
+// Native
+const fs = require('fs')
+const { join } = require('path')
+const { promisify } = require('util')
+const copyFile = promisify(fs.copyFile)
+
+// Modules
 const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+
+require('dotenv-safe').load()
+
+module.exports = withTypescript({
+  publicRuntimeConfig: {
+    // Environment variables are accessible throughout the application now
+    // using "import getConfig from 'next/config'".
+    GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT
+  },
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    if (dev) {
+      return defaultPathMap
+    }
+    // This will copy auth.html from the project root into the 'out' directory.
+    await copyFile(join(dir, 'auth.html'), join(outDir, 'auth.html'))
+    return defaultPathMap
+  }
+})
