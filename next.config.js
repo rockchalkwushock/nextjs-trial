4908573ccfb1f7b6054fe212b885bda9e7dda48c
Cody Brunner <rockchalkwushock@icloud.com>
2018-11-01 14:44:21 -0700

    adds bundle analyzer & filters moment locales.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const { promisify } = require('util')
 const copyFile = promisify(fs.copyFile)
 
 // Modules
+const webpack = require('webpack')
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 const withCSS = require('@zeit/next-css')
 const withTypescript = require('@zeit/next-typescript')
 
@@ -13,23 +15,43 @@ if (process.env.NODE_ENV === 'development') {
   require('dotenv-safe').load()
 }
 
-module.exports = withCSS(
-  withTypescript({
-    publicRuntimeConfig: {
-      // Environment variables are accessible throughout the application now
-      // using "import getConfig from 'next/config'".
-      GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT
-    },
-    exportPathMap: async function(
-      defaultPathMap,
-      { dev, dir, outDir, distDir, buildId }
-    ) {
-      if (dev) {
-        return defaultPathMap
-      }
-      // This will copy auth.html from the project root into the 'out' directory.
-      await copyFile(join(dir, 'auth.html'), join(outDir, 'auth.html'))
+const config = {
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: './bundles/client.html'
+    }
+  },
+  publicRuntimeConfig: {
+    // Environment variables are accessible throughout the application now
+    // using "import getConfig from 'next/config'".
+    GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT
+  },
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    if (dev) {
       return defaultPathMap
     }
-  })
-)
+    // This will copy auth.html from the project root into the 'out' directory.
+    await copyFile(join(dir, 'auth.html'), join(outDir, 'auth.html'))
+    return defaultPathMap
+  },
+  webpack: config => {
+    config.plugins.push(
+      // Cherry picks locales to be included in bundle.
+      // MomentJS adds a significant amount of weight to the overall bundle size.
+      // This is crucial to limiting the bundle size.
+      // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
+      new webpack.ContextReplacementPlugin(
+        /moment[/\\]locale$/,
+        /da|de|en-au|en-gb|es|fr|it|nl|pt|sv/
+      )
+    )
+    return config
+  }
+}
+
+module.exports = withBundleAnalyzer(withCSS(withTypescript(config)))
